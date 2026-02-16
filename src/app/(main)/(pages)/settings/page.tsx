9dd277b2970ea8370689fe9a9a169dadd997b74f
sauriin <saurinparmar2324@gmail.com>
2026-02-16 23:14:52 +0530

    fix: overlapping content

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -55,9 +55,9 @@ const Settings = async (props: Props) => {
     }
 
     return (
-        <div className='flex flex-col gap-4'>
-            <h1 className='sticky top-0 z-10 flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-bg'>
-                <span>Settings</span>
+        <div className="flex flex-col gap-4">
+            <h1 className="p-6 text-4xl border-b bg-background">
+                Settings
             </h1>
             <div className='flex flex-col gap-10 p-6'>
                 <div>
--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -5,12 +5,17 @@ type Props = {}
 
 const Workflows = (props: Props) => {
     return (
-        <div className='flex flex-col gap-4 relative'>
-            <h1 className='text-4xl sticky top-0 z-10 p-6 bg-background/50  backdrop-blur-lg flex items-center border-b justify-between'>
+        // <div className='flex flex-col gap-4 relative'>
+        //     <h1 className='text-4xl sticky top-0 z-10 p-6 bg-background/50  backdrop-blur-lg flex items-center border-b justify-between'>
+        //         Workflows
+
+        //     </h1>
+        // </div>
+        <div className="flex flex-col gap-4">
+            <h1 className="p-6 text-4xl border-b justify-between flex bg-background">
                 Workflows
                 <WorkflowButton />
             </h1>
-
         </div>
     )
 }
